070e2078b84e883116cb04a3383c8c9434c637d4
prempatel3471-max <prempatel3471254@gmail.com>
2026-09-01 20:22:07 +0530

    commit

---
+++ bootstrap/vite-project/src/App.jsx
@@ -0,0 +1,19 @@
+{
+  /* The following line can be included in your src/index.js or App.js file*/
+}
+import './App.css';
+
+function App()
+{ return(
+
+<div className='prem'>
+<h1>prem</h1>
+
+</div>
+
+
+
+)
+}
+
+export default App
